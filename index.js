f10e633a5fb8534456a113e459af261c13584b51
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-12-21 09:23:20 +0100

    added cookie session

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
+const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
@@ -10,6 +12,16 @@ const authRoutes = require("./routes/authRoutes");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
