53a3646fdee27719cbb6f427ee8085a70a07e9ce
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2024-01-05 12:39:36 +0100

    client google auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <a href="/auth/google">Sign In With Google</a>
       </header>
     </div>
   );
