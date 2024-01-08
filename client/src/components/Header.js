dcc371946d3263adbb748fb759692cda1f343c78
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2024-01-08 09:45:30 +0100

    Added Header href

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,10 +5,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Survey</a>
+          <a href="/" className="left brand-logo">
+            Survey
+          </a>
           <ul className="right">
             <li>
-              <a>Login With Google</a>
+              <a href="/">Login With Google</a>
             </li>
           </ul>
         </div>
