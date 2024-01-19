b72073072b4f665344c31c27c04308f8f6f3bcec
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2024-01-19 19:25:03 +0100

    refactor survey field component

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default ({ input, label, meta: { error, touched } }) => {
+const SurveyField = ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
@@ -11,3 +11,5 @@ export default ({ input, label, meta: { error, touched } }) => {
     </div>
   );
 };
+
+export default SurveyField;
