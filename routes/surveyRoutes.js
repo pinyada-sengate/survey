ee17232ca9758c557fcbffe0fbef8adac3762cde
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2024-01-30 14:38:00 +0100

    api surveys webhooks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
+
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const Mailer = require("../services/Mailer");
@@ -39,5 +43,45 @@ module.exports = (app) => {
     } catch (err) {
       res.status(422).send(err);
     }
+  });
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const p = new Path("/api/surveys/:surveyId/:choice");
+
+    const events = req.body.map(({ email, url }) => {
+      const pathName = new URL(url).pathname;
+      const match = p.test(pathName);
+      if (match) {
+        return {
+          email,
+          surveyId: match.surveyId,
+          choice: match.choice,
+        };
+      }
+    });
+
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, "email", "surveyId");
+
+    uniqueEvents.forEach(({ surveyId, email, choice }) => {
+      Survey.updateOne(
+        {
+          _id: surveyId,
+          recipients: {
+            $elemMatch: {
+              email: email,
+              responded: false,
+            },
+          },
+        },
+        {
+          $inc: { [choice]: 1 }, // increment by 1
+          // set responded value from the rescipients that found from serching to true
+          $set: { "recipients.$.responded": true },
+        }
+      ).exec();
+    });
+
+    res.send({});
   });
 };
